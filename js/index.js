88acd26d1713b007db112ad9045bdbb77d2ca822
paako781 <paul-marie.akoffodji@polymtl.ca>
2022-12-28 20:19:00 -0500

    Fix footer link

--- js/index.js
+++ js/index.js
@@ -4,3 +4,9 @@ const navLinks = document.querySelectorAll(".nav__link");
 navToggle.addEventListener("click", () => {
   document.body.classList.toggle("nav-open");
 });
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", () => {
+    document.body.classList.remove("nav-open");
+  });
+});
